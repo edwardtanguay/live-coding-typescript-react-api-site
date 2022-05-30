0768786db5c7cf5c59052eff21cf12bc16a44005
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-30 11:59:28 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-// import { IPerson, Status, PersonStatus } from './data/interfaces';
+import { IBook } from './data/interfaces';
+import { getBooks } from './data/bookModel';
 
 const url = 'https://gutendex.com/books/?search=paris';
 
 function App() {
-	const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState<IBook[]>([]);
 
 	useEffect(() => {
 		(async () => {
-			setBooks((await axios.get(url)).data.results);
+			const rawBooks = (await axios.get(url)).data.results;
+			const _books: IBook[] = getBooks(rawBooks);
+			setBooks(_books);
 		})();
 	}, []);
 
@@ -20,9 +23,11 @@ function App() {
 			<ul>
 				{books.map((book, index) => {
 					return (
-						<li key={index}>{book.title} by {book.authors.length === 0 ? '(unknown)' : book.authors[0].name}</li>
-				)
-			})}
+						<li key={index}>
+							{book.title} by {book.author}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -1,17 +1,4 @@
-export type Status = 'customer' | 'employee' | 'unknown';
-
-export interface IPerson {
-	firstName: string;
-	lastName: string;
-	age: number;
-	isMember: boolean;
-	scores: number[];
-	accessGroups: string[];
-	status: Status; 
-}
-
-export enum PersonStatus {
-	Customer,
-	Employee,
-	Unknown
+export interface IBook {
+	title: string;
+	author: string;
 }
